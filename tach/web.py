41b06e7b7c25d7758b47abf8e7b00e482bd62de1
Leo Singer <leo.singer@ligo.org>
2020-03-09 17:37:12 -0400

    Connect databse to Tornado

--- tach/web.py
+++ tach/web.py
@@ -1,7 +1,8 @@
 from tornado.web import Application
 
 from . import api
+from . import models
 
 __all__ = ('app',)
 
-app = Application(handlers=api.handlers)
+app = Application(handlers=api.handlers, db=models.db)
