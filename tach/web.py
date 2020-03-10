c98f82f81b022036f7f7eb85a5ec4ddb6ff0803c
Leo Singer <leo.singer@ligo.org>
2020-03-10 11:33:21 -0400

    Add npm, webpack

--- tach/web.py
+++ tach/web.py
@@ -1,8 +1,12 @@
-from tornado.web import Application
+from pkg_resources import resource_filename
+from tornado.web import Application, StaticFileHandler
 
 from . import api
 from . import models
 
 __all__ = ('app',)
 
-app = Application(handlers=api.handlers, db=models.db)
+app = Application(
+    [*api.handlers,
+     (r'/static/(.*)', StaticFileHandler, {'path': resource_filename(__name__, 'static')})],
+    db=models.db)
